f962d2b021318d7827fc42af784524c8216b522f
SihagAnkit111 <38473313+SihagAnkit111@users.noreply.github.com>
2018-04-07 23:35:50 +0530

    commit 3

--- A15.cpp
+++ A15.cpp
@@ -89,3 +89,78 @@ void priority_process()
     }
     
 }
+int rtime,rcount;
+void round_robin_process()
+{
+    printf("\n\nRound robin processs are running\n");
+    
+    printf("\n\nprocess no\t|before burst time\t|process remaining time\n"); 
+
+    int flag=0;
+        int min=1000;
+        for(int i=0;i<n[1];i++)
+        {
+            if(que2[i].ar_time<min)
+            {
+                min=que2[i].ar_time;
+                rcount=i;
+            }
+        }
+    while(total_burst[1]!=0)
+    {
+              
+        printf("\npro%d\t\t %d\t\t",rcount+1,que2[rcount].remain_time);
+         
+
+        if(que2[rcount].remain_time<=time_quantum && que2[rcount].remain_time>0) 
+            { 
+                remain_total_burst[0]+=time_quantum-que2[rcount].remain_time;
+            
+            intime+=que2[rcount].remain_time; 
+            total_burst[1]-=que2[rcount].remain_time;
+                
+            que2[rcount].remain_time=0; 
+            flag=1; 
+            } 
+            else if(que2[rcount].remain_time>0) 
+            { 
+            que2[rcount].remain_time-=time_quantum; 
+            total_burst[1]-=time_quantum;
+            intime+=time_quantum; 
+            } 
+
+             printf("\t%d\n",que2[rcount].remain_time);
+            if(que2[rcount].remain_time==0 && flag==1) 
+            { 
+                printf("process %d terminated\n",rcount+1);
+            flag=0; 
+            } 
+
+            if(rcount==n[1]-1) 
+            {
+                rcount=0;
+                while(que2[rcount].remain_time==0)
+                    {
+                        rcount++;
+
+                    }    
+
+            }else if(que2[rcount+1].ar_time<=intime) 
+                {
+                    rcount++;
+                    
+                    while(que2[rcount].remain_time==0)
+                    rcount++; 
+                }
+                else 
+                rcount=0;
+        
+
+        flag=0;
+        min=1000;
+
+    }
+
+
+}
+
